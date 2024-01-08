e22993b92bd6b8651e5f880d5968fa2f852d162e
DNYANDEEP GAONKAR <104913044+dnyandeep23@users.noreply.github.com>
2024-01-08 22:56:34 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: 'class', // enable dark mode
   theme: {
     extend: {},
